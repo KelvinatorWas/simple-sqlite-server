48450877b6e574aa5df50bf4f807cb5bf4641d83
KelvinatorWas <liepkalnskalvis@gmail.com>
2024-04-05 19:10:11 +0300

    removed old functions that are not being used

--- src/main/table.ts
+++ src/main/table.ts
@@ -11,20 +11,6 @@ const tableDrop = (db: Database, tableName: string) => {
   db.run(sql);
 };
 
-const old_tableInsert = (
-  db: Database,
-  tableName: string,
-  sqlData: string[],
-  dataValues: (string | number)[]
-) => {
-  const sql = `INSERT INTO ${tableName} (${sqlData.join(
-    ", "
-  )}) VALUES (${joinData(sqlData)})`;
-  db.run(sql, dataValues, (err) => {
-    hasError(err);
-  });
-};
-
 const tableInsert = (
   db: Database,
   sql: string,
@@ -36,27 +22,6 @@ const tableInsert = (
     if (err?.message) message = "FAILED";
   });
   return message;
-};
-
-const old_tableQuery = (
-  db: Database,
-  tableName: string,
-  sqlData: string[] | string = "*",
-  query = "",
-  params: (string | number)[] = [],
-  setter?: (data: unknown[]) => void
-) => {
-  const sql = `SELECT ${
-    typeof sqlData === "string" ? sqlData : sqlData.join(", ")
-  } FROM ${tableName} ${query}`;
-
-  db.all(sql, params, (err, rows) => {
-    hasError(err);
-
-    if (setter) {
-      setter(rows);
-    }
-  });
 };
 
 const tableQuery = (
@@ -71,19 +36,6 @@ const tableQuery = (
   });
 };
 
-const old_tableUpdate = (
-  db: Database,
-  tableName: string,
-  sqlData: string,
-  dataValues: (string | number)[]
-) => {
-  const sql = `UPDATE ${tableName} SET ${sqlData}`;
-
-  db.run(sql, dataValues, (err) => {
-    hasError(err);
-  });
-};
-
 const tableUpdate = (
   db: Database,
   sql: string,
@@ -95,19 +47,6 @@ const tableUpdate = (
     if (err?.message) message = "FAILED";
   });
   return message;
-};
-
-const old_tableDelete = (
-  db: Database,
-  tableName: string,
-  sqlData: string,
-  dataValues: (string | number)[]
-) => {
-  const sql = `DELETE FROM ${tableName} WHERE ${sqlData}`;
-
-  db.run(sql, dataValues, (err) => {
-    hasError(err);
-  });
 };
 
 const tableDelete = (
